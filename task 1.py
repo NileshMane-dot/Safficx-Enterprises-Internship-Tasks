6f2a825c58ead892a6b62210b25905ef93447f07
NileshMane-dot <nileshmane421501@gmail.com>
2025-08-26 23:09:36 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,19 @@
+import pywhatkit
+import time
+contacts = [
+    ("‪‪+919309238227‬", "Hello!,My self Nilesh Mane")
+    
+]
+
+# start time (2 minutes ahead of current time)
+hour = 21
+minute = 30
+
+#send a message to each number
+for number, message in contacts:
+    pywhatkit.sendwhatmsg(number, message, hour, minute)
+    print(f"sent: {number}")
+    minute += 2  #each message will be forwarded 2 minutes later
+    time.sleep(10)  # wait a while
+
+print("All messages have been sent.")
